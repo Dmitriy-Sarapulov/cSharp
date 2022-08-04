2ead952a0d60ff9052b92fea23820876f9935d99
Dmitriy Sarapulov <sarapulovdima@mail.ru>
2022-08-04 21:44:12 +0300

    Решение задачи

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число и выдает его квадрат
+Console.Clear();
+
+Console.Write("Введите число");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sqrt = number * number;
+//int sqrt = Convert.ToInt32(Math.Pow(number, 2));
+
+Console.Write("Число в квадрате: " + sqrt);
