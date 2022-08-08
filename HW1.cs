b98fbdfd0fcfb61d45b05e04f1754b107f45e3a6
Dmitriy Sarapulov <sarapulovdima@mail.ru>
2022-08-08 17:05:03 +0300

    Решение задачи 6

--- HW1.cs
+++ HW1.cs
@@ -11,20 +11,30 @@
 // }
 
 // Задача 4
-Console.Write("Введите первое число: ");
-int a  = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите второе число: ");
-int b  = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите третье число: ");
-int c  = Convert.ToInt32(Console.ReadLine());
-if (a > b & a > c) {
-    Console.Write("Максимальное число " + a);
+// Console.Write("Введите первое число: ");
+// int a  = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int b  = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите третье число: ");
+// int c  = Convert.ToInt32(Console.ReadLine());
+// if (a > b & a > c) {
+    // Console.Write("Максимальное число " + a);
+// }
+// if (b > a & b > c) {
+    // Console.Write("Максимальное число " + b);
+// }
+// if (c > a & c > b) {
+    // Console.Write("Максимальное число " + c);
+// }
+
+// Задача 6
+Console.Write("Введите число: ");
+int value  = Convert.ToInt32(Console.ReadLine());
+if ((value % 2) == 0) {
+    Console.Write("Данное число явяляется четным");
 }
-if (b > a & b > c) {
-    Console.Write("Максимальное число " + b);
-}
-if (c > a & c > b) {
-    Console.Write("Максимальное число " + c);
+else{
+    Console.Write("Данное число не является четным");
 }
 
 
