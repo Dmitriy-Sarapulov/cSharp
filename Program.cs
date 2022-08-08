331fac95a20d5d7d51514261bab214bdf1ed22b7
Dmitriy Sarapulov <sarapulovdima@mail.ru>
2022-08-08 16:24:28 +0300

    Решение задачи3

--- Program.cs
+++ Program.cs
@@ -11,13 +11,57 @@
 // Console.Write("Число в квадрате: " + sqrt);
 
 // Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число 
-Console.Write("Input first number: ");
-int num1  = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input second number:");
-int num2  = Convert.ToInt32(Console.ReadLine());
-int square = num2 * num2;
-if(num1 == square)
-    Console.Write("First number is a square of second number");
+// Console.Write("Input first number: ");
+// int num1  = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input second number:");
+// int num2  = Convert.ToInt32(Console.ReadLine());
+// int square = num2 * num2;
+// if(num1 == square)
+// Console.Write("First number is a square of second number");
+// else
+// Console.Write("First number is not a square of second number");
+
+//Напишите программу котрая будет выдавать название дня недели по заданному номеру
+
+Console.Write("Input number of the day: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if (day < 1 || day > 7)
+{
+    Console.Write("Incorrect number");
+}
+
 else
-    Console.Write("First number is not a square of second number");
-        
+{
+    if (day == 1)
+    {
+        Console.Write("Monday");
+    }
+    if (day == 2)
+    {
+        Console.Write("Tuesday");
+    }
+    if (day == 3)
+    {
+        Console.Write("Wednesday");
+    }
+    if (day == 4)
+    {
+        Console.Write("Thursday");
+    }
+    if (day == 5)
+    {
+        Console.Write("Friday");
+    }
+    if (day == 6)
+    {
+        Console.Write("Saturday");
+    }
+    if (day == 7)
+    {
+        Console.Write("Sunday");
+    }
+}
+
+
+
