9797fe729a87a1713edfb8752f9739b0bc69824c
Dmitriy Sarapulov <sarapulovdima@mail.ru>
2022-08-08 16:53:14 +0300

    Решение задачи 4

--- HW1.cs
+++ HW1.cs
@@ -1,10 +1,31 @@
+// Задача 2
+// Console.Write("Введите первое число: ");
+// int num1  = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int num2  = Convert.ToInt32(Console.ReadLine());
+// if (num1 > num2) {
+    // Console.Write("Максимальное число " + num1);
+// }
+// else {
+    // Console.Write("Максимальное число " + num2);
+// }
+
+// Задача 4
 Console.Write("Введите первое число: ");
-int num1  = Convert.ToInt32(Console.ReadLine());
+int a  = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
-int num2  = Convert.ToInt32(Console.ReadLine());
-if (num1 > num2) {
-    Console.Write("Максимальное число " + num1);
+int b  = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int c  = Convert.ToInt32(Console.ReadLine());
+if (a > b & a > c) {
+    Console.Write("Максимальное число " + a);
 }
-else {
-    Console.Write("Максимальное число " + num2);
+if (b > a & b > c) {
+    Console.Write("Максимальное число " + b);
 }
+if (c > a & c > b) {
+    Console.Write("Максимальное число " + c);
+}
+
+
+
