92e12d95cd060cc145d55999d203184b99cba935
Dmitriy Sarapulov <sarapulovdima@mail.ru>
2022-08-11 21:21:31 +0300

    Задача с координатами

--- lesson3.cs
+++ lesson3.cs
@@ -1,1 +1,22 @@
 //Функции и массивы второй семинар
+
+Console.WriteLine ("Введите координату х: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите координату y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > 0 && y > 0) {
+    Console.WriteLine("Первая четверть");
+}
+else if (x < 0 && y > 0) {
+    Console.WriteLine("Вторая четверть");
+}
+else if (x < 0 && y < 0) {
+    Console.WriteLine("Третья четверть");
+}
+else if (x > 0 && y < 0) {
+    Console.WriteLine("Четвертая четверть");
+}
+else {
+    Console.WriteLine("Невозможно определить четверть");
+}
