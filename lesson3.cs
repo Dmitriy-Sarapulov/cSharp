282db5dc655359d718d68b23ab50b2aabce042a0
Dmitriy Sarapulov <sarapulovdima@mail.ru>
2022-08-11 21:24:10 +0300

    Задача с координатами 2

--- lesson3.cs
+++ lesson3.cs
@@ -1,22 +1,44 @@
 //Функции и массивы второй семинар
 
-Console.WriteLine ("Введите координату х: ");
-int x = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine ("Введите координату y: ");
-int y = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine ("Введите координату х: ");
+// int x = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine ("Введите координату y: ");
+// int y = Convert.ToInt32(Console.ReadLine());
 
-if (x > 0 && y > 0) {
-    Console.WriteLine("Первая четверть");
-}
-else if (x < 0 && y > 0) {
-    Console.WriteLine("Вторая четверть");
-}
-else if (x < 0 && y < 0) {
-    Console.WriteLine("Третья четверть");
-}
-else if (x > 0 && y < 0) {
-    Console.WriteLine("Четвертая четверть");
-}
-else {
-    Console.WriteLine("Невозможно определить четверть");
-}
+// if (x > 0 && y > 0) {
+//     Console.WriteLine("Первая четверть");
+// }
+// else if (x < 0 && y > 0) {
+//     Console.WriteLine("Вторая четверть");
+// }
+// else if (x < 0 && y < 0) {
+//     Console.WriteLine("Третья четверть");
+// }
+// else if (x > 0 && y < 0) {
+//     Console.WriteLine("Четвертая четверть");
+// }
+// else {
+//     Console.WriteLine("Невозможно определить четверть");
+// }
+
+//Второй способ решения через swich case
+// Console.Write("Введите координаты точки X: ");
+// int x = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите координаты точки Y: ");
+// int y = int.Parse(Console.ReadLine()!);
+// string mes = "";
+// if (x == 0) 
+// Console.Write("введите отличное от 0");
+// if (y == 0) 
+// Console.Write("введите отличное от 0");
+// switch (x, y)
+// {
+//     case (>0, >0): mes = "1"; break;
+//     case (>0, <0): mes = "4"; break;
+//     case (<0, >0): mes = "2"; break;
+//     case (<0, <0): mes = "3"; break;
+// }
+// Console.WriteLine(mes);
+
+
+
