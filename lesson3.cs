b83466f817fe0d7f40a67aec870c0eb4caad40ca
Dmitriy Sarapulov <sarapulovdima@mail.ru>
2022-08-11 21:56:08 +0300

    Задача с координатами 3

--- lesson3.cs
+++ lesson3.cs
@@ -30,15 +30,54 @@
 // if (x == 0) 
 // Console.Write("введите отличное от 0");
 // if (y == 0) 
-// Console.Write("введите отличное от 0");
+// Console.Write("введите отличное от 0"); // либо так
 // switch (x, y)
 // {
 //     case (>0, >0): mes = "1"; break;
 //     case (>0, <0): mes = "4"; break;
 //     case (<0, >0): mes = "2"; break;
 //     case (<0, <0): mes = "3"; break;
+//     default : mes = "введите отличное от 0"; break; //либо так
+ 
 // }
 // Console.WriteLine(mes);
+
+// Напишите программу, которая по заданному номеру четверти,
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+// Console.Write("Введите номер четверти: ");
+// int quarter=int.Parse(Console.ReadLine());
+
+// switch (quarter)
+// {
+//     case 1:
+//     {
+//         Console.WriteLine("x>0 y>0");
+//         break;
+//     }
+//     case 2:
+//     {
+//         Console.WriteLine("x<0 y>0");
+//         break;
+//     }
+//     case 3:
+//     {
+//         Console.WriteLine("x<0 y<0");
+//         break;
+//     }
+//     case 4:
+//     {
+//         Console.WriteLine("x>0 y<0");
+//         break;
+//     }
+    
+//     default:
+//     {
+//         Console.WriteLine("Введена неправильная четверть");
+//         break;
+//     }
+// }
+
 
 
 
