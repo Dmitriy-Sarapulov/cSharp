aaa73299b7b1973effa5c0b7d991131a071e316a
Dmitriy Sarapulov <sarapulovdima@mail.ru>
2022-08-17 16:15:00 +0300

    Задача 21

--- HW3.cs
+++ HW3.cs
@@ -1,13 +1,34 @@
 // Задача 19
-Console.Write("Введите пятизначное число: ");
-int a = Convert.ToInt32(Console.ReadLine());
-string b = Convert.ToString(a);
-if(b.Length != 5) {
-    Console.WriteLine("Введено не верное значение!");
-}
-else if (b[0] == b[4] && b[1] == b[3]) {
-    Console.WriteLine("Данное число является палиндромом");
-}
-else {
-    Console.WriteLine("Данное число не является палиндромом");
-}
+// Console.Write("Введите пятизначное число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// string b = Convert.ToString(a);
+// if(b.Length != 5) {
+    // Console.WriteLine("Введено не верное значение!");
+// }
+// else if (b[0] == b[4] && b[1] == b[3]) {
+    // Console.WriteLine("Данное число является палиндромом");
+// }
+// else {
+    // Console.WriteLine("Данное число не является палиндромом");
+// }
+
+// Задача 21
+
+// Console.Write("Введите X1: ");
+// int x1=int.Parse(Console.ReadLine());
+// Console.Write("Введите Y1: ");
+// int y1=int.Parse(Console.ReadLine());
+// Console.Write("Введите Z1: ");
+// int z1=int.Parse(Console.ReadLine());
+// Console.Write("Введите X2: ");
+// int x2=int.Parse(Console.ReadLine());
+// Console.Write("Введите Y2: ");
+// int y2=int.Parse(Console.ReadLine());
+// Console.Write("Введите Z2: ");
+// int z2=int.Parse(Console.ReadLine());
+
+// double d = Math.Sqrt(Math.Pow(x1-x2,2) + Math.Pow(y1-y2,2) + Math.Pow(z1-z2,2));
+
+// Console.WriteLine($"d={d:f2}");
+
+//Задача 23
