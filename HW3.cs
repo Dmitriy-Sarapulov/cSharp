6bf9efcefbc6ec0692ebf73c193696ad8aa32b2b
Dmitriy Sarapulov <sarapulovdima@mail.ru>
2022-08-17 15:48:47 +0300

    Задача 19

--- HW3.cs
+++ HW3.cs
@@ -1,1 +1,13 @@
 // Задача 19
+Console.Write("Введите пятизначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+string b = Convert.ToString(a);
+if(b.Length != 5) {
+    Console.WriteLine("Введено не верное значение!");
+}
+else if (b[0] == b[4] && b[1] == b[3]) {
+    Console.WriteLine("Данное число является палиндромом");
+}
+else {
+    Console.WriteLine("Данное число не является палиндромом");
+}
