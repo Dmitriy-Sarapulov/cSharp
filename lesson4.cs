1c6ad3f512da80366d241e80ae228d700c120f53
Dmitriy Sarapulov <sarapulovdima@mail.ru>
2022-08-18 17:21:37 +0300

    задача 1

--- lesson4.cs
+++ lesson4.cs
@@ -1,1 +1,15 @@
 //Функции продолжение
+//Программа принимает на вход число А и выдает сумму чисел от 1 до А.
+// int GetSumNums(int number) {
+//     int sum = 0;
+//     for(int i = 1;i <= number; i++) {
+//         sum += i;
+//     }
+//     return sum;
+// }
+
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int num = int.Parse(Console.ReadLine());
+// Console.WriteLine($"Сумма цифр = {GetSumNums(num)}");
+
