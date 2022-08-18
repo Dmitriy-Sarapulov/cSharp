8efc58bdf9b37f45b1f8aef24d0b3384f6d2b197
Dmitriy Sarapulov <sarapulovdima@mail.ru>
2022-08-18 17:41:11 +0300

    задача 3

--- lesson4.cs
+++ lesson4.cs
@@ -15,29 +15,35 @@
 
 //Программа которая принимает на вход число и выдает количество цифр в числе.
 
-Console.Write("Введите число: ");
-int num = Convert.ToInt32(Console.ReadLine());
-int index = 0;
-if(num == 0)
-{
-    Console.Write("1");
-} else {
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int index = 0;
+// if(num == 0)
+// {
+//     Console.Write("1");
+// } else {
 
-    while(num != 0) {
-        num = num / 10;
-        index++;
-    }
-    Console.WriteLine(index);
-}
-
-
-
-
-
-
+//     while(num != 0) {
+//         num = num / 10;
+//         index++;
+//     }
+//     Console.WriteLine(index);
+// }
 
 // Программа которая принимает на вход число N и выдает произведение чисел от 1 до N
 
+int GetSumNums(int number) {
+    int sum = 1;
+    for(int i = 1;i <= number; i++) {
+        sum *= i;
+    }
+    return sum;
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма цифр = {GetSumNums(num)}");
 
 
 
