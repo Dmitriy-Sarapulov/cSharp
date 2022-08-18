0d18dd65aefaf2431864eda469215fd9fe829523
Dmitriy Sarapulov <sarapulovdima@mail.ru>
2022-08-18 17:37:47 +0300

    задача 2

--- lesson4.cs
+++ lesson4.cs
@@ -13,3 +13,34 @@
 // int num = int.Parse(Console.ReadLine());
 // Console.WriteLine($"Сумма цифр = {GetSumNums(num)}");
 
+//Программа которая принимает на вход число и выдает количество цифр в числе.
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int index = 0;
+if(num == 0)
+{
+    Console.Write("1");
+} else {
+
+    while(num != 0) {
+        num = num / 10;
+        index++;
+    }
+    Console.WriteLine(index);
+}
+
+
+
+
+
+
+
+// Программа которая принимает на вход число N и выдает произведение чисел от 1 до N
+
+
+
+
+
+
+// Программа которая выводит массив из 8 элементов,заполненный нулями и единицами в случайном порядке.
