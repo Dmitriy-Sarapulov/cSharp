e014e82855699cc9ef450b4cc6f004145a67abdf
Dmitriy Sarapulov <sarapulovdima@mail.ru>
2022-08-25 13:45:23 +0300

    задача 29

--- HW4.cs
+++ HW4.cs
@@ -29,3 +29,10 @@
 
 //Задача 29
 
+// int[] arr = new int[8];
+// Random rand = new Random();
+// for(int i = 0; i < arr.Length; i++) {
+//     arr[i] = rand.Next(0, 100);
+//     Console.Write(arr[i] + " ");
+// }
+
