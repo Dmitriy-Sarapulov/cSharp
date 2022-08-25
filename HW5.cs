17eeb1d957df51d994ad0f2e7b634e0183347828
Dmitriy Sarapulov <sarapulovdima@mail.ru>
2022-08-25 15:57:31 +0300

    задача 34

--- HW5.cs
+++ HW5.cs
@@ -1,1 +1,21 @@
 //ДЗ 5
+
+// Задача 34
+
+// Console.Write("Введите размер массива: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// int[] arr = new int[a];
+// Random rand = new Random();
+// for(int i = 0; i < arr.Length; i++) {
+//     arr[i] = rand.Next(100, 1000);
+//     Console.Write(arr[i] + " ");
+// }
+// int chet = 0;
+// for(int i = 0; i < arr.Length; i++) {
+//     if(arr[i] % 2 == 0) {
+//         chet = chet + 1;
+//     }
+    
+// }
+// Console.Write(chet);
