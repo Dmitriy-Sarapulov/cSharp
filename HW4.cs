edce1a2ccd9181f7e55957b182c8a6c8be0c86d3
Dmitriy Sarapulov <sarapulovdima@mail.ru>
2022-08-25 12:25:08 +0300

    задача 25

--- HW4.cs
+++ HW4.cs
@@ -1,1 +1,9 @@
 //ДЗ4
+//Задача 25:
+
+// Console.Write("Введите число А: ");
+// double a = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите число B: ");
+// double b = Convert.ToDouble(Console.ReadLine());
+// double step = Math.Pow(a,b);
+// Console.WriteLine(step);
