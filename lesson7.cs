58416c5b3da94107dbadddb1cb2f457a0b207ef4
Dmitriy Sarapulov <sarapulovdima@mail.ru>
2022-09-22 18:39:38 +0300

    Ф-я вывода массива

--- lesson7.cs
+++ lesson7.cs
@@ -1,15 +1,29 @@
 // Пара полезных функций от другого препода.
 
 
-int [,] FillArray(int numLine, int numColums, int maxRand = 20, int minRand = 0 )   //Функция создания двумерного массива
-{
-    int[,] matrix = new int[numLine, numColums];
-    for(int i = 0; i < matrix.GetLength(0); i++)    //Строка
-    {
-        for(int j = 0; j < matrix.GetLength(1); j++)    //Столбец
-        {
-            matrix[i,j] = new Random().Next(minRand, maxRand);  // [0;20] по умолчанию
-        }
-    }
-    return matrix;
-}
+// int [,] FillArray(int numLine, int numColums, int maxRand = 20, int minRand = 0 )   //Функция создания двумерного массива
+// {
+//     int[,] matrix = new int[numLine, numColums];
+//     for(int i = 0; i < matrix.GetLength(0); i++)    //Строка
+//     {
+//         for(int j = 0; j < matrix.GetLength(1); j++)    //Столбец
+//         {
+//             matrix[i,j] = new Random().Next(minRand, maxRand);  // [0;20] по умолчанию
+//         }
+//     }
+//     return matrix;
+// }
+
+// void PrintArray(int[,] matrix) //Функция вывода массива.
+// {
+//     for(int i =0; i < matrix.GetLength(0); i++)
+//     {
+//         for(int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             Console.Write($"{matrix[i, j]}\t");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+
