088502ffb49ff4d472e9d0e4dbe2086dda24c0d2
Dmitriy Sarapulov <sarapulovdima@mail.ru>
2022-09-22 18:29:32 +0300

    Ф-я создания 2-у мерного массива

--- lesson7.cs
+++ lesson7.cs
@@ -1,1 +1,15 @@
 // Пара полезных функций от другого препода.
+
+
+int [,] FillArray(int numLine, int numColums, int maxRand = 20, int minRand = 0 )   //Функция создания двумерного массива
+{
+    int[,] matrix = new int[numLine, numColums];
+    for(int i = 0; i < matrix.GetLength(0); i++)    //Строка
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)    //Столбец
+        {
+            matrix[i,j] = new Random().Next(minRand, maxRand);  // [0;20] по умолчанию
+        }
+    }
+    return matrix;
+}
